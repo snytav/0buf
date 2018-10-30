bd1f3b25ff1064a23ef39d5814abff65cb44113a
snytav <snytav@gmail.com>
2018-10-30 19:20:32 +0700

    returned 5,5,5 in order to investigate

--- gpu_plasma.h
+++ gpu_plasma.h
@@ -1789,7 +1789,7 @@ int StepAllCells(int nt,double mass,double q_mass)
 	   cudaStatus = cudaLaunchKernel(
 	                                            (const void*)GPU_CurrentsAllCells, // pointer to kernel func.
 	                                            dimGrid,                       // grid
-	                                            dimBlock1,                      // block
+	                                            dimBlockExt,                      // block
 	                                            args1,                          // arguments
 	                                            0,
 	                                            0
